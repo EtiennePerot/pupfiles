2690ff470649e5651782a89fbc6db169328688fe
Etienne Perot <etienne@perot.me>
2013-01-18 18:44:22 -0500

    Simplify aur_package module

--- modules/aur_package/manifests/init.pp
+++ modules/aur_package/manifests/init.pp
@@ -1,10 +1,9 @@
 define aur_package (
 	$ensure = installed
 ) {
-	include aur_package::yaourt
+	require aur_package::yaourt
 	package {$title:
 		name => "$name",
-		ensure => $ensure,
-		require => Package['yaourt']
+		ensure => $ensure
 	}
 }
