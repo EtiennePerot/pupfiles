a65cd7f4c725bf02cc3a1e566b74df8e10fbadb7
Etienne Perot <etienne@perot.me>
2013-01-18 16:55:21 -0500

    Allow this.manifest to not exist if manifest name matches machine hostname

--- util/local-apply.sh
+++ util/local-apply.sh
@@ -10,10 +10,13 @@ fi
 export PUP_JUST_SET_VARIABLES='true'
 source "$scriptDir/setup.sh"
 unset PUP_JUST_SET_VARIABLES
-if [ ! -f this.manifest ]; then
-	echo 'Cannot find this.manifest. Make sure everything is set up properly first.' >&2
-	exit 1
+chosenManifest=$(hostname)
+if [ ! -f "manifests/$chosenManifest.pp" ]; then
+	if [ ! -f this.manifest ]; then
+		echo 'Cannot find this.manifest. Make sure everything is set up properly first.' >&2
+		exit 1
+	fi
+	read chosenManifest < this.manifest
 fi
-read chosenManifest < this.manifest
 modulePath="$pupDir/private/modules:$pupDir/modules:`puppet apply --configprint modulepath`"
 exec puppet apply --modulepath "$modulePath" "manifests/$chosenManifest.pp"
