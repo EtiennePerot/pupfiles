91c428fdf3efd5130cbad4024e338d7e8b6d95dc
Etienne Perot <etienne@perot.me>
2013-01-18 18:44:44 -0500

    Make systemd_service module require the systemd package

--- modules/systemd_service/manifests/symlink_hack.pp
+++ modules/systemd_service/manifests/symlink_hack.pp
@@ -1,7 +1,11 @@
 class systemd_service::symlink_hack {
 	# Workaround for Puppet bug https://projects.puppetlabs.com/issues/18129
+	if ! defined(Package['systemd']) {
+		package {'systemd':}
+	}
 	file {'/bin/systemctl':
 		ensure => link,
-		target => '/usr/bin/systemctl'
+		target => '/usr/bin/systemctl',
+		require => Package['systemd']
 	}
 }
