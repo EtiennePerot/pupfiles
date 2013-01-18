e0f6e46e70e2a9ba4f0844ce5cea86c328a508cd
Etienne Perot <etienne@perot.me>
2013-01-18 16:54:40 -0500

    Add some games

--- modules/desktop/manifests/games.pp
+++ modules/desktop/manifests/games.pp
@@ -1,3 +1,11 @@
 class desktop::games {
-	# TODO
+	aur_package {'wine-multimedia':}
+	package {'wine_gecko':
+		require => Aur_package['wine-multimedia']
+	}
+	package {'winetricks':
+		require => Aur_package['wine-multimedia']
+	}
+	aur_package {'steam':}
+	package {'xonotic':}
 }
