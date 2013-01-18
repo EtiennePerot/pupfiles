32fd3d8f9d00ed179bf7fc167d3391485f3bd3db
Etienne Perot <etienne@perot.me>
2013-01-18 15:49:56 -0500

    Inline syntax highlighting for shell

--- modules/console/manifests/oh_my_zsh.pp
+++ modules/console/manifests/oh_my_zsh.pp
@@ -1,11 +1,18 @@
 class console::oh_my_zsh {
 	include console::zsh
 	aur_package {'oh-my-zsh-git':}
+	aur_package {'zsh-completions':}
+	aur_package {'zsh-syntax-highlighting':}
 	include base::etienne
 	include base::packaging::pkgfile
 	enduser_file {'.zshrc':
 		source => 'console/zsh',
-		require => [Package['zsh'], Class['base::packaging::pkgfile'], Enduser_file['.zsh']]
+		require => [
+			Package['zsh'],                         # Actual shell
+			Class['base::packaging::pkgfile'],      # Required for not-found hook
+			Aur_package['zsh-syntax-highlighting'], # Required for syntax highlighting
+			Enduser_file['.zsh']                    # Parent directory
+		]
 	}
 	enduser_file {'.zsh':
 		ensure => directory,
