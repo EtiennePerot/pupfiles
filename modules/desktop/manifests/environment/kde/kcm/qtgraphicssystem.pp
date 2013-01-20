e749e97ae9753ca0addb9df9b6518e556f7e6de6
Etienne Perot <etienne@perot.me>
2013-01-20 00:51:13 -0500

    Set qt graphics render to raster

--- modules/desktop/manifests/environment/kde/kcm/qtgraphicssystem.pp
+++ modules/desktop/manifests/environment/kde/kcm/qtgraphicssystem.pp
@@ -1,3 +1,13 @@
 class desktop::environment::kde::kcm::qtgraphicssystem {
 	aur_package {'kcm-qt-graphicssystem':}
+	if ! defined(Enduser_file['.kde4/env']) {
+		enduser_file {'.kde4/env':
+			ensure => directory
+		}
+	}
+	enduser_file {'.kde4/env/qt-graphicssystem.sh':
+		filename => 'qt-graphicssystem.sh',
+		mode => 0700,
+		source => 'desktop/environment/kde/kcm/qtgraphicssystem'
+	}
 }
