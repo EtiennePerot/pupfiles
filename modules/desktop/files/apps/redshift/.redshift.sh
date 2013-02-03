54fc46012a814bf544d2c5b676c261a1942dcaca
Etienne Perot <etienne@perot.me>
2013-02-03 17:24:33 -0500

    Add redshift autostarting script

---
+++ modules/desktop/files/apps/redshift/.redshift.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+redshift -l 45.507:-73.649
--- modules/desktop/manifests/apps/redshift.pp
+++ modules/desktop/manifests/apps/redshift.pp
@@ -1,9 +1,13 @@
 class desktop::apps::redshift {
 	aur_package {'redshift':}
-	enduser_file {'.redshift':
+	enduser_file {'.redshift.sh':
 		ensure => present,
+		mode => 0700,
 		source => 'desktop/apps/redshift',
 		require => Aur_package['redshift']
 	}
-	# TODO: Startup file
+	kde_autostart {'.redshift.sh':
+		simplename => 'redshift.sh',
+		require => Enduser_file['.redshift.sh']
+	}
 }
