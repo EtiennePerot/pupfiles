8de5fe8590af23db3f6a3ef9c56495246d03cec3
Etienne Perot <etienne@perot.me>
2013-02-06 18:04:32 -0500

    Add xkeyboard configuration

--- modules/desktop/manifests/environment/kde/kcm/input.pp
+++ modules/desktop/manifests/environment/kde/kcm/input.pp
@@ -2,4 +2,7 @@ class desktop::environment::kde::kcm::input {
 	kde_rc::merge {'kcminputrc':
 		source => 'desktop/environment/kde/kcm/input'
 	}
+	kde_rc::merge {'kxkbrc':
+		source => 'desktop/environment/kde/kcm/input'
+	}
 }
