087519d26b92af7c0fd7221817e421991693e4c9
Etienne Perot <etienne@perot.me>
2013-02-11 13:06:17 -0500

    Add wallpapers to desktop module

--- util/gen-private-layout.sh
+++ util/gen-private-layout.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
+ignoredPatterns=(
+	'private/modules/private/templates/console/gpg/*.asc'
+	'private/modules/private/files/desktop/wallpapers/*'
+)
+
 scriptDir=$(dirname "$BASH_SOURCE")
 cd "$scriptDir/.."
-layout=$(find private -path '*/console/gpg/*.asc' -prune -o -type f -print)
+findArguments=()
+layoutExtra=''
+for pattern in "${ignoredPatterns[@]}"; do
+	findArguments+=(-path "$pattern" -prune -o)
+	layoutExtra="$layoutExtra
+$pattern"
+done
+layout=$(find private "${findArguments[@]}" -type f -print)
 layout="$layout
-private/modules/private/templates/console/gpg/*.asc"
+$layoutExtra"
 layout=$(echo "$layout" | sed -r 's#([^/]+)@([^./]+)\.([^./]+)#\1 (at) \2 (dot) \3#g')
-echo "$layout" | sort > private-layout.txt
+echo "$layout" | sort | grep . > private-layout.txt
