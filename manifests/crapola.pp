6ef04f20623f1838bc7d1067c7119a76a015b445
Etienne Perot <etienne@perot.me>
2013-02-11 15:31:49 -0500

    Make crapola a synergy client, and add its network interface info

--- manifests/crapola.pp
+++ manifests/crapola.pp
@@ -6,3 +6,19 @@ class {'desktop':
 	laptop => true,
 	graphics_drivers => 'intel'
 }
+
+network_interface {$ethernet_macaddress:
+	rename => 'wired0',
+	macspoof => ['wired_laptop'],
+	ipv6_privacy => true
+}
+
+network_interface {$wifi_macaddress:
+	rename => 'wifi0',
+	macspoof => ['wireless_laptop', 'wireless_usb'],
+	ipv6_privacy => true
+}
+
+class {'synergy::client':
+	server => 'Blorb2.local'
+}
