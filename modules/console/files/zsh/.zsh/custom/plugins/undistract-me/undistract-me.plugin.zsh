d083524462f13029b19f6b18e009a8d677343874
Etienne Perot <etienne@perot.me>
2013-02-13 20:19:29 -0500

    Add command blacklist to undistract-me for which no notification should happen

--- modules/console/files/zsh/.zsh/custom/plugins/undistract-me/undistract-me.plugin.zsh
+++ modules/console/files/zsh/.zsh/custom/plugins/undistract-me/undistract-me.plugin.zsh
@@ -1,3 +1,13 @@
+UNDISTRACT_ME_CMD_BLACKLIST=(
+	screen
+	ssh
+	man
+	wine
+	less
+	more
+	most
+)
+
 undistract_me_preexec() {
 	UNDISTRACT_ME_CMD=${${${(pj:\\\\n:)${(f)1}}//\%/%%}//\\/\\\\}
 	UNDISTRACT_ME_CMD_START_TIME=$(date +%s)
@@ -6,9 +16,18 @@ undistract_me_precmd() {
 	local end_time duration
 	if [ "$UNDISTRACT_ME_CMD" != '' ]; then
 		end_time=$(date +%s)
-		((duration=$end_time - $UNDISTRACT_ME_CMD_START_TIME))
-		if [ "$duration" -gt 15 ]; then
-			notify-send --urgency=low --expire-time=1500 --icon=utilities-terminal 'Command executed' "'$UNDISTRACT_ME_CMD' ended in $duration seconds."
+		validCommand='true'
+		for cmd in "${UNDISTRACT_ME_CMD_BLACKLIST[@]}"; do
+			if echo "$UNDISTRACT_ME_CMD" | grep -qP "^(?:sudo)?\s*$cmd\b"; then
+				validCommand=''
+				break
+			fi
+		done
+		if [ -n "$validCommand" ]; then
+			((duration=$end_time - $UNDISTRACT_ME_CMD_START_TIME))
+			if [ "$duration" -gt 15 ]; then
+				notify-send --urgency=low --expire-time=1500 --icon=utilities-terminal 'Command executed' "'$UNDISTRACT_ME_CMD' ended in $duration seconds."
+			fi
 		fi
 	fi
 	unset UNDISTRACT_ME_CMD
