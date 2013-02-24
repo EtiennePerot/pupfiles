490317ea56da5b8165e7e7fa9a5932c91baa1a2a
Etienne Perot <etienne@perot.me>
2013-02-23 19:19:29 -0500

    Revert to default config

--- modules/base/manifests/timekeeping.pp
+++ modules/base/manifests/timekeeping.pp
@@ -1,24 +1,6 @@
-class base::timekeeping (
-	$source = 'perot.me',
-	$port = 443,
-	$proxy = 'none',
-	$cert = 'certs/perot.me.crt'
-) {
+class base::timekeeping {
 	require certs
 	aur_package {'tlsdate':}
-	file {'/etc/tlsdate':
-		ensure => directory,
-		require => Aur_package['tlsdate']
-	}
-	file {'/etc/tlsdate/tlsdated.conf':
-		content => template('base/timekeeping/tlsdated.conf.erb')
-	}
-	file {'/etc/tlsdate/ca-roots':
-		ensure => directory
-	}
-	file {'/etc/tlsdate/ca-roots/tlsdate-ca-roots.conf':
-		content => file('/usr/share/ca-certificates/mozilla/StartCom_Certification_Authority.crt')
-	}
 	systemd_service {'tlsdated':
 		enable => true,
 		require => Aur_package['tlsdate']
