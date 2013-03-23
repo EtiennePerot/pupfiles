1ad63a73d773fecdb591ad5c88996842e77bb3bd
Etienne Perot <etienne@perot.me>
2013-03-23 17:54:49 -0400

    Add namcap package

--- modules/base/manifests/packaging.pp
+++ modules/base/manifests/packaging.pp
@@ -1,6 +1,7 @@
 class base::packaging {
 	package {'pacman':}
 	package {'abs':}
+	package {'namcap':}
 	include base::packaging::pkgfile
 	include base::packaging::multilib
 	include base::packaging::armh
