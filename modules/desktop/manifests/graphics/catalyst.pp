ece1005591e3b1d970ce3f0e6eb75998a87e4062
Etienne Perot <etienne@perot.me>
2013-04-17 01:45:01 -0400

    Add opencl-catalyst when installing ATI graphics drivers

--- modules/desktop/manifests/graphics/catalyst.pp
+++ modules/desktop/manifests/graphics/catalyst.pp
@@ -4,6 +4,7 @@ class desktop::graphics::catalyst {
 		require => Class['base::kernel::dkms']
 	}
 	package {'catalyst-utils':}
+	package {'opencl-catalyst':}
 	include base::packaging::multilib
 	package {'lib32-catalyst-utils':
 		require => Class['base::packaging::multilib']
