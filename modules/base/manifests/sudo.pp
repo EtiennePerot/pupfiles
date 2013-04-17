94436c22038b47bde62065693f07e9367d21e744
Etienne Perot <etienne@perot.me>
2013-04-17 01:30:03 -0400

    Add /etc/sudoers.d/defaults_keep_home

--- modules/base/manifests/sudo.pp
+++ modules/base/manifests/sudo.pp
@@ -6,4 +6,10 @@ class base::sudo {
 		content => '%wheel ALL=(ALL) NOPASSWD: ALL',
 		require => Package['sudo']
 	}
+	file {'/etc/sudoers.d/defaults_keep_home':
+		ensure => present,
+		content => "Defaults env_keep += \"HOME\"\n", # Line at the end is important, otherwise sudo will die completely
+		require => Package['sudo'],
+		mode => 0440 # Important, otherwise sudo will die completely
+	}
 }
