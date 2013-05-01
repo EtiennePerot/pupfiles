6be100f51fa84bbbad5abfaecf8d77ea5f230362
Etienne Perot <etienne@perot.me>
2013-05-01 18:22:45 -0400

    Add support for .cron.sh to take an optional argument to skip the sleep (useful to force a mail check to happen right now).
    Also 15 minutes is actually 900 seconds

--- modules/pop3_maildir/manifests/init.pp
+++ modules/pop3_maildir/manifests/init.pp
@@ -10,7 +10,7 @@ define pop3_maildir (
 	$sieve = '',
 	$cron_hour = '*',
 	$cron_minute = '*/20',
-	$cron_randomdelay = 54000, # 15 minutes = 54000 seconds
+	$cron_randomdelay = 900, # 15 minutes = 900 seconds
 	$torify = true
 ) {
 	require pop3_maildir::restore_stub_dir
