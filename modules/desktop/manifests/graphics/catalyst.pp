1c566cfe5719b7abcdbca32f44eef4faca0dee8d
Etienne Perot <etienne@perot.me>
2013-05-03 15:18:11 -0400

    Replace catalyst-dkms with catalyst-hook

--- modules/desktop/manifests/graphics/catalyst.pp
+++ modules/desktop/manifests/graphics/catalyst.pp
@@ -1,10 +1,6 @@
 class desktop::graphics::catalyst {
-	include base::kernel::dkms
-	package {'catalyst-dkms':
-		require => Class['base::kernel::dkms']
-	}
+	package {'catalyst-hook':}
 	package {'catalyst-utils':}
-	package {'opencl-catalyst':}
 	include base::packaging::multilib
 	package {'lib32-catalyst-utils':
 		require => Class['base::packaging::multilib']
