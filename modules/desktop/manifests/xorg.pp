b2d83b1bd3400067a558dac16c7f72ea3ae3d04b
Etienne Perot <etienne@perot.me>
2013-05-03 15:47:00 -0400

    Add .xcompose from kragen/xcompose

--- modules/desktop/manifests/xorg.pp
+++ modules/desktop/manifests/xorg.pp
@@ -5,4 +5,7 @@ class desktop::xorg {
 	package {'xorg-xmodmap':}
 	package {'xorg-mkfontdir':}
 	package {'xorg-mkfontscale':}
+	enduser_file {'.XCompose':
+		source => 'desktop/xorg/xcompose/dotXCompose'
+	}
 }
