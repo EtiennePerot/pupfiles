448919c1994524de27fd48876689592524469af6
Etienne Perot <etienne@perot.me>
2013-05-05 02:31:55 -0400

    Only create .sync mount when doing so is valid

--- modules/desktop/manifests/sync.pp
+++ modules/desktop/manifests/sync.pp
@@ -4,29 +4,29 @@ class desktop::sync {
 		ensure => directory,
 		noroot => true
 	}
-	if ! $__custom_has_dot_sync {
+	if inline_template('<%= File.exists?("/home/etienne/.sync-decrypt") %>') == 'false' {
+		# Need to check for non-existence so that if .sync-decrypt is a mountpoint, it doesn't get trashed
 		enduser_file {'.sync-decrypt':
 			ensure => directory,
 			noroot => true
 		}
 	}
-	enduser_file {'.sync/encfs':
-		ensure => directory,
-		noroot => true
-	}
-	file {'/usr/bin/DotSyncMount':
-		mode => 0755,
-		owner => 'root',
-		group => 'root',
-		source => 'puppet:///modules/desktop/sync/DotSyncMount'
-	}
-	package {'encfs':}
-	mount {'/home/etienne/.sync-decrypt':
-		ensure => defined,
-		atboot => true,
-		device => 'DotSyncMount#_',
-		fstype => 'fuse',
-		options => 'auto,defaults',
-		require => [Package['encfs'], File['/usr/bin/DotSyncMount']]
+	if inline_template('<%= File.exists?("/home/etienne/.sync/encfs") %>') == 'true' {
+		# Only define this once synchronization has kicked in, which is true when the encfs subdirectory exists
+		file {'/usr/bin/DotSyncMount':
+			mode => 0755,
+			owner => 'root',
+			group => 'root',
+			source => 'puppet:///modules/desktop/sync/DotSyncMount'
+		}
+		package {'encfs':}
+		mount {'/home/etienne/.sync-decrypt':
+			ensure => defined,
+			atboot => true,
+			device => 'DotSyncMount#_',
+			fstype => 'fuse',
+			options => 'auto,defaults',
+			require => [Package['encfs'], File['/usr/bin/DotSyncMount']]
+		}
 	}
 }
