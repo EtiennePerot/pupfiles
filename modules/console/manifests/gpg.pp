aa5d03354d7be267cd8dea77a54d8a44bf07388a
Etienne Perot <etienne@perot.me>
2013-05-29 20:56:59 -0400

    Add May First/People Link keyserver certificate

--- modules/console/manifests/gpg.pp
+++ modules/console/manifests/gpg.pp
@@ -1,6 +1,11 @@
 class console::gpg {
+	file {'/etc/ssl/certs/mayfirst-peoplelink.crt':
+		source => 'puppet:///modules/console/gpg/mayfirst-peoplelink.crt',
+		mode => 0444
+	}
 	enduser_file {'.gnupg/gpg.conf':
-		source => 'console/gpg'
+		source => 'console/gpg',
+		require => File['/etc/ssl/certs/mayfirst-peoplelink.crt']
 	}
 
 	include private::console::gpg
