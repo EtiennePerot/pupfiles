2fa877710228d30fce3465002e9ba53c172d316f
Etienne Perot <etienne@perot.me>
2013-06-07 15:48:29 -0400

    Remove now-unecessary systemctl symlink hack

--- modules/systemd_service/manifests/symlink_hack.pp
+++ modules/systemd_service/manifests/symlink_hack.pp
@@ -1,11 +1,11 @@
 class systemd_service::symlink_hack {
-	# Workaround for Puppet bug https://projects.puppetlabs.com/issues/18129
 	if ! defined(Package['systemd']) {
 		package {'systemd':}
 	}
-	file {'/bin/systemctl':
-		ensure => link,
-		target => '/usr/bin/systemctl',
-		require => Package['systemd']
-	}
+	# Workaround for Puppet bug https://projects.puppetlabs.com/issues/18129 - Not needed anymore!
+	#file {'/bin/systemctl':
+	#	ensure => link,
+	#	target => '/usr/bin/systemctl',
+	#	require => Package['systemd']
+	#}
 }
