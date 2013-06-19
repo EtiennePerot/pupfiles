87177a497d1f4fa79592217c744d4bb5d2183668
Etienne Perot <etienne@perot.me>
2013-06-19 18:11:06 -0400

    Set DuckDuckGo as default search engine when searching from the URL bar as well

--- modules/browser/manifests/firefox/duckduckgo.pp
+++ modules/browser/manifests/firefox/duckduckgo.pp
@@ -2,4 +2,7 @@ class browser::firefox::duckduckgo {
 	firefox_pref {'browser.search.selectedEngine':
 		value => 'DuckDuckGo'
 	}
+	firefox_pref {'keyword.URL':
+		value => 'https://duckduckgo.com/?q='
+	}
 }
